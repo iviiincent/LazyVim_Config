c707ba91c63a3e54b68929459627616cc0d54eaa
Vincent M <keel_direst_0r@icloud.com>
2024-11-30 22:36:32 +0800

    Modify <tab> behavior

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -14,8 +14,24 @@ return {
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-          -- cmp.select_next_item()
-          cmp.confirm({ select = true })
+          cmp.select_next_item()
+        elseif vim.snippet.active({ direction = 1 }) then
+          vim.schedule(function()
+            vim.snippet.jump(1)
+          end)
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif vim.snippet.active({ direction = -1 }) then
+          vim.schedule(function()
+            vim.snippet.jump(-1)
+          end)
         else
           fallback()
         end
